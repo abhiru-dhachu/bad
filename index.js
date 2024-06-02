4091328f442a5fad006e82ee6c7e9173f5233d87
mask ser <444shijil@mail.com>
2024-06-02 16:10:03 +0530

    fix

--- index.js
+++ index.js
@@ -1,204 +1,192 @@
-const {
-  default: makeWASocket,
-  useMultiFileAuthState,
-  DisconnectReason,
-  Browsers,
-  fetchLatestBaileysVersion,
-  delay,
-  loadSession,
-  makeCacheableSignalKeyStore,
-  makeInMemoryStore
-} = require("@adiwajshing/baileys");
-const fs = require("fs");
-const path = require("path");
-const pino = require("pino");
-const config = require("./config");
-const io = require("socket.io-client");
-const axios = require("axios");
-const {
-  loadMessage,
-  saveMessage,
-  saveChat,
-} = require("./lib/database/store");
-const { MakeSession } = require("./lib/session");
-const { Message, commands, numToJid, sudoIds, PREFIX } = require("./lib/index");
-const { serialize } = require("./lib/serialize");
-const store = makeInMemoryStore({
-  logger: pino().child({ level: "silent", stream: "store" }),
-});
-// Set global variable for base directory
-global.__basedir = __dirname;
-
-// Function to read and require JavaScript files from a directory
-const readAndRequireFiles = async (directory) => {
-  try {
-    const files = await fs.promises.readdir(directory);
-    return Promise.all(
-      files
-        .filter((file) => path.extname(file).toLowerCase() === ".js")
-        .map((file) => require(path.join(directory, file)))
-    );
-  } catch (error) {
-    console.error("Error reading and requiring files:", error);
-    throw error; // Rethrow the error for higher-level handling
-  }
-};
-
-// Main function to connect and initialize
-async function initialize() {
-if (!fs.existsSync("./lib/session/creds.json")) {
-  await MakeSession(config.SESSION_ID, "lib/session");
-  console.log("Vesrion : " + require("./package.json").version)
-}
-  console.log("WhatsApp Bot Initializing...");
-
-    await readAndRequireFiles(path.join(__dirname, "/lib/database"));
-
-    // Sync database
-    await config.DATABASE.sync();
-    console.log("Database synchronized.");
-
-    // Read and require plugin files
-    console.log("Installing Plugins...");
-    await readAndRequireFiles(path.join(__dirname, "/plugins"));
-    console.log("Plugins Installed!");
-
-// Function to connect to WhatsApp
-async function connectToWhatsApp() {
-  try {
-    console.log("Connecting to WhatsApp...");
-    const {
-    state,
-    saveCreds
-  } = await useMultiFileAuthState("./lib/session/");
-
-    const { version } = await fetchLatestBaileysVersion();
-    const logger = pino({ level: "silent" });
-    const client = makeWASocket({
-      logger,
-      printQRInTerminal: false,
-      downloadHistory: false,
-      syncFullHistory: false,
-      browser: Browsers.macOS("Desktop"),
-      auth: {
-        creds: state.creds,
-        keys: makeCacheableSignalKeyStore(state.keys, logger),
-      },
-      version,
-    });
-
-    client.ev.on("connection.update", async (node) => {
-      const { connection, lastDisconnect } = node;
-      if (connection === "open") {
-        console.log("Connected to WhatsApp.");
-        const sudo = numToJid(config.SUDO.split(",")[0]) || client.user.id;
-        await client.sendMessage(sudo, {
-          text: `*BOT CONNECTED*\n\nPREFIX: ${PREFIX}\nPLUGINS: ${
-            commands.filter((command) => command.pattern).length
-          }\nVERSION: ${require("./package.json").version}`,
-        });
-      }
-      if (
-        connection === "close" &&
-        lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut
-      ) {
-        console.log("Reconnecting...");
-        await delay(300);
-        connectToWhatsApp();
-      } else if (connection === "close") {
-        console.log("Connection closed.");
-        await delay(3000);
-        process.exit(0);
-      }
-    });
-
-    client.ev.on("creds.update", saveCreds);
-
-    client.ev.on("messages.upsert", async (upsert) => {
-      if (!upsert.type === "notify") return;
-      msg = upsert.messages[0];
-    await serialize(JSON.parse(JSON.stringify(msg)), client);
-     await saveMessage(upsert.messages[0], msg.sender);
-      if (!msg.message) return;
-      const message = new Message(client, msg);
-      if (config.LOG_MSG && !message.data.key.fromMe)
-        console.log(
-          `[MESSAGE] [${message.pushName || message.sender.split("@")[0]}] : ${
-            message.text || message.type || null
-          }`
-        );
-      if (
-        config.READ_MSG == true &&
-        message.data.key.remoteJid !== "status@broadcast"
-      )
-        await client.readMessages([message.data.key]);
-        const isBot = (message.fromMe && message.id.startsWith('BAE5') && message.id.length == 12) || (message.fromMe && message.id.startsWith('BAE5') && message.id.length === 16);
-if (!(!isBot || (isBot && message.text && /(kick|warn|dlt)$/.test(message.text)))) {
-    return;
-}
-      commands.map(async (command) => {
-        const messageType = {
-          image: "imageMessage",
-          sticker: "stickerMessage",
-          audio: "audioMessage",
-          video: "videoMessage",
-        };
-
-        const isMatch =
-          (command.on &&
-            messageType[command.on] &&
-            message.msg &&
-            message.msg[messageType[command.on]] !== null) ||
-          !command.pattern ||
-          command.pattern.test(message.text) ||
-          (command.on === "text" && message.text) ||
-          (command.on && !messageType[command.on] && !message.msg[command.on]);
-
-        if (isMatch) {
-          if (command.fromMe && !message.isSudo) return;
-          if (command.onlyPm && message.isGroup) return;
-          if (command.onlyGroup && !message.isGroup) return;
-          if (command.pattern && config.READ_CMD == true)
-            await client.readMessages([message.data.key]);
-          const match = message.text?.match(command.pattern) || "";
-
-          try {
-            await command.function(
-              message,
-              match.length === 6 ? match[3] ?? match[4] : match[2] ?? match[3],
-              client,
-            );
-          } catch (e) {
-            if (config.ERROR_MSG) {
-              console.log(e);
-              const sudo =
-                numToJid(config.SUDO.split(",")[0]) || client.user.id;
-              await client.sendMessage(
-                sudo,
-                {
-                  text:
-                    "```─━❲ ERROR REPORT ❳━─\n\nMessage : " +
-                    message.text +
-                    "\nError : " +
-                    e.message +
-                    "\nJid : " +
-                    message.jid +
-                    "```",
-                },
-                { quoted: message.data }
-              );
-            }
-          }
-        }
-      });
-    });
-    return client;
-  } catch (error) {
-    console.error("Error connecting to WhatsApp:", error);
-    process.exit(1); // Exit with error status
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  DisconnectReason,
+  Browsers,
+  fetchLatestBaileysVersion,
+  delay,
+  makeCacheableSignalKeyStore,
+  makeInMemoryStore
+} = require("@adiwajshing/baileys");
+const fs = require("fs");
+const path = require("path");
+const pino = require("pino");
+const config = require("./config");
+const { MakeSession } = require("./lib/session");
+const { Message, commands, numToJid, PREFIX } = require("./lib/index");
+const { serialize } = require("./lib/serialize");
+const store = makeInMemoryStore({
+  logger: pino().child({ level: "silent", stream: "store" }),
+});
+
+global.__basedir = __dirname;
+
+const readAndRequireFiles = async (directory) => {
+  try {
+    const files = await fs.promises.readdir(directory);
+    return Promise.all(
+      files
+        .filter((file) => path.extname(file).toLowerCase() === ".js")
+        .map((file) => require(path.join(directory, file)))
+    );
+  } catch (error) {
+    console.error("Error reading and requiring files:", error);
+    throw error;
   }
-  connectToWhatsApp();
-}
-
-// Call the initialization function
-initialize();
+};
+
+async function initialize() {
+  if (!fs.existsSync("./lib/session/creds.json")) {
+    await MakeSession(config.SESSION_ID, "lib/session");
+    console.log("Version : " + require("./package.json").version);
+  }
+  console.log("WhatsApp Bot Initializing...");
+
+  await readAndRequireFiles(path.join(__dirname, "/lib/database"));
+
+  await config.DATABASE.sync();
+  console.log("Database synchronized.");
+
+  console.log("Installing Plugins...");
+  await readAndRequireFiles(path.join(__dirname, "/plugins"));
+  console.log("Plugins Installed!");
+
+  async function connectToWhatsApp() {
+    try {
+      console.log("Connecting to WhatsApp...");
+      const { state, saveCreds } = await useMultiFileAuthState("./lib/session/");
+
+      const { version } = await fetchLatestBaileysVersion();
+      const logger = pino({ level: "silent" });
+      const client = makeWASocket({
+        logger,
+        printQRInTerminal: false,
+        downloadHistory: false,
+        syncFullHistory: false,
+        browser: Browsers.macOS("Desktop"),
+        auth: {
+          creds: state.creds,
+          keys: makeCacheableSignalKeyStore(state.keys, logger),
+        },
+        version,
+      });
+
+      client.ev.on("connection.update", async (node) => {
+        const { connection, lastDisconnect } = node;
+        if (connection === "open") {
+          console.log("Connected to WhatsApp.");
+          const sudo = numToJid(config.SUDO.split(",")[0]) || client.user.id;
+          await client.sendMessage(sudo, {
+            text: `*BOT CONNECTED*\n\nPREFIX: ${PREFIX}\nPLUGINS: ${
+              commands.filter((command) => command.pattern).length
+            }\nVERSION: ${require("./package.json").version}`,
+          });
+        }
+        if (
+          connection === "close" &&
+          lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut
+        ) {
+          console.log("Reconnecting...");
+          await delay(300);
+          connectToWhatsApp();
+        } else if (connection === "close") {
+          console.log("Connection closed.");
+          await delay(3000);
+          process.exit(0);
+        }
+      });
+
+      client.ev.on("creds.update", saveCreds);
+
+      client.ev.on("messages.upsert", async (upsert) => {
+        if (upsert.type !== "notify") return;
+        const msg = upsert.messages[0];
+        await serialize(JSON.parse(JSON.stringify(msg)), client);
+        await saveMessage(upsert.messages[0], msg.sender);
+        if (!msg.message) return;
+        const message = new Message(client, msg);
+        if (config.LOG_MSG && !message.data.key.fromMe)
+          console.log(
+            `[MESSAGE] [${message.pushName || message.sender.split("@")[0]}] : ${
+              message.text || message.type || null
+            }`
+          );
+        if (
+          config.READ_MSG == true &&
+          message.data.key.remoteJid !== "status@broadcast"
+        )
+          await client.readMessages([message.data.key]);
+        const isBot = (message.fromMe && message.id.startsWith('BAE5') && message.id.length == 12) || (message.fromMe && message.id.startsWith('BAE5') && message.id.length === 16);
+        if (!(!isBot || (isBot && message.text && /(kick|warn|dlt)$/.test(message.text)))) {
+          return;
+        }
+        commands.map(async (command) => {
+          const messageType = {
+            image: "imageMessage",
+            sticker: "stickerMessage",
+            audio: "audioMessage",
+            video: "videoMessage",
+          };
+
+          const isMatch =
+            (command.on &&
+              messageType[command.on] &&
+              message.msg &&
+              message.msg[messageType[command.on]] !== null) ||
+            !command.pattern ||
+            command.pattern.test(message.text) ||
+            (command.on === "text" && message.text) ||
+            (command.on && !messageType[command.on] && !message.msg[command.on]);
+
+          if (isMatch) {
+            if (command.fromMe && !message.isSudo) return;
+            if (command.onlyPm && message.isGroup) return;
+            if (command.onlyGroup && !message.isGroup) return;
+            if (command.pattern && config.READ_CMD == true)
+              await client.readMessages([message.data.key]);
+            const match = message.text?.match(command.pattern) || "";
+
+            try {
+              await command.function(
+                message,
+                match.length === 6 ? match[3] ?? match[4] : match[2] ?? match[3],
+                client,
+              );
+            } catch (e) {
+              if (config.ERROR_MSG) {
+                console.log(e);
+                const sudo =
+                  numToJid(config.SUDO.split(",")[0]) || client.user.id;
+                await client.sendMessage(
+                  sudo,
+                  {
+                    text:
+                      "```─━❲ ERROR REPORT ❳━─\n\nMessage : " +
+                      message.text +
+                      "\nError : " +
+                      e.message +
+                      "\nJid : " +
+                      message.jid +
+                      "```",
+                  },
+                  { quoted: message.data }
+                );
+              }
+            }
+          }
+        });
+      });
+      return client;
+    } catch (error) {
+      console.error("Error connecting to WhatsApp:", error);
+      throw error;
+    }
+  }
+
+  await connectToWhatsApp();
+}
+
+initialize().catch((error) => {
+  console.error("Initialization failed:", error);
+  process.exit(1);
+});
