af9bbdbdb4f2967b1c3fa7cf767632f63148dbf1
mask ser <appushijil72@gmail.com>
2024-07-16 08:02:33 +0530

    added take,fix tag,ffmpeg funcyions

--- plugins/media_tools.js
+++ plugins/media_tools.js
@@ -1,12 +1,12 @@
 const config = require("../config");
-const { Rudhra, mode, toAudio } = require("../lib/");
-
+const { Rudhra, mode, toAudio,blackVideo,getFfmpegBuffer,audioCut,videoTrim } = require("../lib/");
+const fs = require('fs');
 Rudhra(
   {
     pattern: "photo",
     fromMe: mode,
     desc: "Converts sticker to photo",
-    type: "converter",
+    type: "media",
   },
   async (message, match, client) => {
     if (!message.reply_message || !message.reply_message.sticker) {
@@ -20,7 +20,7 @@ Rudhra(
   pattern: "mp3",
   fromMe: mode,
   desc: "converts video/voice to mp3",
-  type: "downloader",
+  type: "media",
 }, async (message, match) => {
   try {
     let buff = await message.quoted.download("buffer");
@@ -35,6 +35,187 @@ Rudhra(
     );
   } catch (error) {
     console.error("Error:", error);
-    return await message.sendMessage("An error occurred while processing your request.");
+    return await message.send("An error occurred while processing your request.");
   }
 });
+Rudhra(
+  { pattern: 'black', fromMe: mode, desc: 'Audio to black screen video.', type: 'media' },
+  async (message, match) => {
+    
+    if (!message.reply_message || !message.reply_message.audio) {
+      return await message.reply('*Reply to an audio!*');
+    }
+
+    try {
+      const audioPath = await message.quoted.download();
+      const videoBuffer = await blackVideo(audioPath);
+      await message.sendFile(videoBuffer,{ quoted: message.data});
+    } catch (error) {
+      console.error('Error processing black video:', error);
+      await message.reply(`Failed to create black video. Error: ${error.message}`);
+    }
+  }
+);
+Rudhra({ pattern: 'avec', fromMe: mode, desc: 'audio editor',type:"media" }, async (message, match) => {
+  if (!message.reply_message || !message.reply_message.audio)
+    return await message.send('*Reply to a audio.*')
+  return await message.sendFile(
+    await getFfmpegBuffer(
+      await message.reply_message.download(),
+      'avec.mp4',
+      'avec'
+    ),
+    {quoted: message.data },
+   )
+})
+ Rudhra({ pattern: 'pitch', fromMe: mode, desc: 'audio editor',type: "media"}, async (message, match) => {
+  if (!message.reply_message || !message.reply_message.audio)
+    return await message.send('*Reply to a audio.*')
+  return await message.sendMsg(
+    await getFfmpegBuffer(
+      await message.reply_message.download(),
+      'lowmp3.mp3',
+      'lowmp3'
+    ),
+    { filename: 'lowmp3.mp3', mimetype: 'audio/mpeg', quoted: message.data },'audio'  )
+})
+Rudhra({ pattern: 'low', fromMe: mode, desc: 'audio editor',type: "media"}, async (message, match) => {
+  if (!message.reply_message || !message.reply_message.audio)
+    return await message.send('*Reply to a audio.*')
+  return await message.sendMsg(
+    await getFfmpegBuffer(
+      await message.reply_message.download(),
+      'lowmp3.mp3',
+      'pitch'
+    ),
+    { filename: 'lowmp3.mp3', mimetype: 'audio/mpeg', quoted: message.data },
+    'audio'
+  )
+})
+Rudhra({ pattern: 'vector', fromMe: mode, desc: 'video editor',type: "media"}, async (message, match) => {
+  if (!message.reply_message || !message.reply_message.audio)
+    return await message.send('*Reply to a audio.*')
+  return await message.sendMsg(
+    await getFfmpegBuffer(
+      await message.reply_message.download(),
+      'vector.mp4',
+      'vector'
+    ),
+    { mimetype: 'video/mp4', quoted: message.data },
+    'video'
+  )
+})
+Rudhra(
+  { pattern: 'compress ?(.*)', fromMe: mode, desc: 'video compressor',type: "media" },
+  async (message, match) => {
+    if (!message.reply_message || !message.reply_message.video)
+      return await message.send('*Reply to a video*')
+    return await message.sendMsg(
+      await getFfmpegBuffer(
+        await message.reply_message.download(),
+        'ocompress.mp4',
+        'compress'
+      ),
+      { quoted: message.data },
+      'video'
+    )
+  }
+)
+
+Rudhra(
+  { pattern: 'bass ?(.*)', fromMe: mode, desc: 'audio editor',type: "media"},
+  async (message, match) => {
+    if (!message.reply_message || !message.reply_message.audio)
+      return await message.send('*Reply to a audio.*')
+    return await message.sendMsg(
+      await getFfmpegBuffer(
+        await message.reply_message.download(),
+        'bass.mp3',
+        `bass,${match == '' ? 10 : match}`
+      ),
+      { mimetype: 'audio/mpeg', quoted: message.data },
+      'audio'
+    )
+  }
+)
+
+Rudhra(
+  { pattern: 'treble ?(.*)', fromMe: mode, desc: 'audio editor', type: "media"},
+  async (message, match) => {
+    if (!message.reply_message || !message.reply_message.audio)
+      return await message.send('*Reply to a audio.*')
+    return await message.sendMsg(
+      await getFfmpegBuffer(
+        await message.reply_message.download(),
+        'treble.mp3',
+        `treble,${match == '' ? 10 : match}`
+      ),
+      { mimetype: 'audio/mpeg', quoted: message.data },
+      'audio'
+    )
+  }
+);
+
+Rudhra({ pattern: 'histo', fromMe: mode, desc: 'audio editor',type: "media" }, async (message, match) => {
+  if (!message.reply_message || !message.reply_message.audio)
+    return await message.send('*Reply to a audio.*')
+  return await message.sendMsg(
+    await getFfmpegBuffer(
+      await message.reply_message.download(),
+      'histo.mp4',
+      'histo'
+    ),
+    { mimetype: 'video/mp4', quoted: message.data },
+    'video'
+  )
+});
+Rudhra({ pattern: 'reverse', fromMe: mode, desc: 'reverse video or audio', type: "media"}, async (message, match) => {
+  if (
+    !message.reply_message.audio &&
+    !message.reply_message.video &&
+    !message.reply_message
+  )
+    return await message.send('*Reply to video/audio*')
+  const location = await message.reply_message.download()
+  if (message.reply_message.video == true) {
+    return await message.sendMsg(
+      await getFfmpegBuffer(location, 'revered.mp4', 'videor'),
+      { mimetype: 'video/mp4', quoted: message.data },
+      'video'
+    )
+  } else if (message.reply_message.audio == true) {
+    return await message.sendMsg(
+      await getFfmpegBuffer(location, 'revered.mp3', 'audior'),
+      {
+        filename: 'revered.mp3',
+        mimetype: 'audio/mpeg',
+        ptt: false,
+        quoted: message.data,
+      },
+      'audio'
+    )
+  }
+})
+
+Rudhra({ pattern: 'cut ?(.*)', fromMe: mode, desc: 'cut audio',type: "media"}, async (message, match) => {
+  if (!message.reply_message || !message.reply_message.audio)
+    return await message.send('*Reply to a audio.*')
+  if (!match) return await message.send('*Example : cut 0;30*')
+  const [start, duration] = match.split(';')
+  if (!start || !duration || isNaN(start) || isNaN(duration))
+    return await message.send('*Example : cut 10;30*')
+  return await message.sendMsg(
+    await audioCut(
+      await message.reply_message.download(),
+      start.trim(),
+      duration.trim()
+    ),
+    {
+      filename: 'cut.mp3',
+      mimetype: 'audio/mpeg',
+      ptt: false,
+      quoted: message.data,
+    },
+    'audio'
+  )
+})
